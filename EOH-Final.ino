37550e3b9c2f3c8796ff1987fd5dbec7531ac927
wrickord <71193259+wrickord@users.noreply.github.com>
2021-04-01 01:32:44 -0500

    Add files via upload

--- EOH-Final.ino
+++ EOH-Final.ino
@@ -8,9 +8,9 @@ Servo pinky;
 
 int position = 0;
 int thumbPin = 8;
-int pointerPin = 9;
-int middleRingPin = 10;
-int pinkyPin = 11;
+int pointerPin = 11;
+int middleRingPin = 9;
+int pinkyPin = 10;
 
 void setup() {
   Serial.begin(9600);
@@ -26,13 +26,10 @@ void setup() {
   pinky.write(position);
 
   Serial.println("Key: ");
-  Serial.println("0 = Reset");
-  Serial.println("1 = Open");
-  Serial.println("2 = Close");
-  Serial.println("3 = Motion Hand");
-  Serial.println("4 = Motion Fingers");
-  Serial.println("5 = Thumbs Up");
-  Serial.println("6 = Number 1");
+  Serial.println("0 = Open");
+  Serial.println("1 = Close");
+  Serial.println("2 = Motion Hand");
+  Serial.println("3/4 = Motion Fingers");
 }
 
 void loop() {
@@ -42,47 +39,30 @@ void loop() {
     input = Serial.read();
 
     if (input == '0') {
-      Serial.println("Resetting...");
-      reset();
-    }
-
-    if (input == '1') {
       Serial.println("Opening...");
       openHand();
     }
 
-    if (input == '2') {
+    if (input == '1') {
       Serial.println("Closing...");
       closeHand();
     }
 
-    if (input == '3') {
+    if (input == '2') {
       Serial.println("Motioning Hand...");
       motionHand();
     }
 
-    if (input == '4') {
-      Serial.println("Motioning Fings...");
+    if (input == '3') {
+      Serial.println("Motioning Fingers...");
       motionFingers();
-    }    
-    
-    if (input == '5') {
-      Serial.println("Thumbs up...");
-      thumbsUp();
     }
 
-    if (input == '6') {
-      Serial.println("Number 1...");
-      numberOne();
-    }    
+    if (input == '4') {
+      Serial.println("Motioning Fingers...");
+      motionFingersTwo();
+    }
   }
-}
-
-void reset() {
-  thumb.write(0);
-  pointer.write(0);
-  middleRing.write(0);
-  pinky.write(0);
 }
 
 void openHand() {
@@ -118,32 +98,35 @@ void motionHand() {
 }
 
 void motionFingers() {
-  reset();
-  for(int x = 0; x < 3; x++) {
-    for (position = 0; position < 90; position++) {
-      pointer.write(position);
-      middleRing.write(position);
-      pinky.write(position);
-    }
-    
-    for (position = 90; position > 0; position--) {
-      pointer.write(position);
-      middleRing.write(position);
-      pinky.write(position);
-    }
+  for (position = 180; position > 0; position--) {
+    pointer.write(position);
+    delay(15);
+  }
+
+  for (position = 180; position > 0; position--) {
+    middleRing.write(position);
+    delay(15);
+  }
+  
+  for (position = 180; position > 0; position--) {
+    pinky.write(position);
+    delay(15);
   }
 }
 
-void thumbsUp() {
-  thumb.write(180);
-  pointer.write(0);
-  middleRing.write(0);
-  pinky.write(0);
-}
+void motionFingersTwo() {
+  for (position = 0; position < 180; position++) {
+    pointer.write(position);
+    delay(15);
+  }
 
-void numberOne() {
-  thumb.write(180);
-  pointer.write(0);
-  middleRing.write(180);
-  pinky.write(180);
+  for (position = 0; position < 180; position++) {
+    middleRing.write(position);
+    delay(15);
+  }
+  
+  for (position = 0; position < 180; position++) {
+    pinky.write(position);
+    delay(15);
+  }
 }
